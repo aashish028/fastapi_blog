7868be60754fd35008f3e77653029ff29b5174e2
Aashish Lalwani <lalwaniaashish28@gmail.com>
2024-05-13 17:31:01 +0530

    Initial Commit

---
+++ backend/core/config.py
@@ -0,0 +1,4 @@
+class Settings:
+    PROJECT_TITLE: str = "blog"
+    PROJECT_VERSION: str = "0.1.0"
+    
---
+++ backend/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from core.config import Settings
+app = FastAPI(title=Settings.PROJECT_TITLE, version=Settings.PROJECT_VERSION)
+@app.get("/")
+def hello():
+    return {'msg':"hello fast"}
